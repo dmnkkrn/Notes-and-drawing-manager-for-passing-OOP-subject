3009aba28c9a936036fe47744382244cfd0112fc
unknown <korniluk24153@stud.akademiabialska.pl>
2026-02-11 16:44:33 +0100

    meow

--- Menedżer notatek i rysunków/Persistence/AutosaveService.cs
+++ Menedżer notatek i rysunków/Persistence/AutosaveService.cs
@@ -4,28 +4,14 @@ using System.IO;
 
 namespace Menedżer_notatek_i_rysunków.Persistence
 {
-    public class AutosaveService<T> : IDisposable
+    public class AutosaveService : IDisposable
     {
         private readonly System.Windows.Forms.Timer _timer;
-        private readonly string _mainPath;
-        private readonly string _restorePath;
-        private readonly string _workBackup;
-        private readonly Func<IEnumerable<T>> _getData;
-        private readonly Action<string, IEnumerable<T>> _save;
+        private readonly Action _saveAction;
 
-        public AutosaveService(
-            string mainPath,
-            string restorePath,
-            string workBackup,
-            Func<IEnumerable<T>> getData,
-            Action<string, IEnumerable<T>> saveAction,
-            int intervalMs)
+        public AutosaveService(Action saveAction, int intervalMs)
         {
-            _mainPath = mainPath;
-            _restorePath = restorePath;
-            _getData = getData;
-            _save = saveAction;
-            _workBackup = workBackup;
+            _saveAction = saveAction;
 
             _timer = new System.Windows.Forms.Timer();
             _timer.Interval = intervalMs;
@@ -34,44 +20,12 @@ namespace Menedżer_notatek_i_rysunków.Persistence
 
         private void OnTick(object sender, EventArgs e)
         {
-            var data = _getData();
-            _save(_restorePath, data);
+            _saveAction();
         }
 
         public void Start() => _timer.Start();
 
         public void Stop() => _timer.Stop();
-
-        public void SaveUser()
-        {
-            var data = _getData();
-            _save(_mainPath, data);
-            _save(_restorePath, data);
-        }
-
-        public bool HasUnsavedChanges()
-        {
-            if (!File.Exists(_mainPath) || !File.Exists(_restorePath))
-                return false;
-
-            var a = File.ReadAllBytes(_mainPath);
-            var b = File.ReadAllBytes(_restorePath);
-
-            if (a.Length != b.Length)
-                return true;
-
-            for (int i = 0; i < a.Length; i++)
-                if (a[i] != b[i])
-                    return true;
-
-            return false;
-        }
-
-        public void DiscardRestore()
-        {
-            if (File.Exists(_restorePath))
-                File.Delete(_restorePath);
-        }
 
         public void Dispose()
         {
